b0b106cb6d406db3655b711e04498b0a17ca0f2b
ivy010210 <ivy010210@gmail.com>
2017-08-29 15:31:46 +0800

    EVENT連資料庫

---
+++ NewEvent/app/src/main/java/com/example/ivy/newevent/Config.java
@@ -0,0 +1,14 @@
+package com.example.ivy.newevent;
+
+/**
+ * Created by ivy on 2017/8/23.
+ */
+
+public class Config {
+    public static final String URL_CREATE_GROUP = "http://140.117.71.114/beacon/createEvent.php";
+
+
+
+    //employee id to pass with intent
+    public static final String EMP_ID = "emp_id";
+}
